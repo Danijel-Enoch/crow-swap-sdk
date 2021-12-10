defd30252281028f96147fb8f38955970fbf5715
coderighter2 <24811616+coderighter2@users.noreply.github.com>
2021-12-09 21:50:37 -0800

    add dist

---
+++ dist/constants.d.ts
@@ -0,0 +1,36 @@
+import JSBI from 'jsbi';
+export declare type BigintIsh = JSBI | bigint | string;
+export declare enum ChainId {
+    MAINNET = 56,
+    TESTNET = 97,
+    CRONOSTESTNET = 338
+}
+export declare enum TradeType {
+    EXACT_INPUT = 0,
+    EXACT_OUTPUT = 1
+}
+export declare enum Rounding {
+    ROUND_DOWN = 0,
+    ROUND_HALF_UP = 1,
+    ROUND_UP = 2
+}
+export declare const FACTORY_ADDRESS = "0xB0b1d323a2A4950dDfdD44F35671516BC4986612";
+export declare const INIT_CODE_HASH = "0x4b79ce49ed81fea5264e35c2533dee5fcdd6a2261e78da994c40facc823dc396";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const TWO: JSBI;
+export declare const THREE: JSBI;
+export declare const FIVE: JSBI;
+export declare const TEN: JSBI;
+export declare const _100: JSBI;
+export declare const FEES_NUMERATOR: JSBI;
+export declare const FEES_DENOMINATOR: JSBI;
+export declare enum SolidityType {
+    uint8 = "uint8",
+    uint256 = "uint256"
+}
+export declare const SOLIDITY_TYPE_MAXIMA: {
+    uint8: JSBI;
+    uint256: JSBI;
+};
