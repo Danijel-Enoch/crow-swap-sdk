0fe34bbd788b8affc8d0f3473e8f3189ed2f0569
coderighter2 <24811616+coderighter2@users.noreply.github.com>
2021-12-15 00:48:31 -0800

    rebuild

--- dist/sdk.esm.js
+++ dist/sdk.esm.js
@@ -761,7 +761,7 @@ var Pair = /*#__PURE__*/function () {
   function Pair(tokenAmountA, tokenAmountB) {
     var tokenAmounts = tokenAmountA.token.sortsBefore(tokenAmountB.token) // does safety checks
     ? [tokenAmountA, tokenAmountB] : [tokenAmountB, tokenAmountA];
-    this.liquidityToken = new Token(tokenAmounts[0].token.chainId, Pair.getAddress(tokenAmounts[0].token, tokenAmounts[1].token), 18, 'Cake-LP', 'Pancake LPs');
+    this.liquidityToken = new Token(tokenAmounts[0].token.chainId, Pair.getAddress(tokenAmounts[0].token, tokenAmounts[1].token), 18, 'Crow-LP', 'Crow LPs');
     this.tokenAmounts = tokenAmounts;
   }
 
